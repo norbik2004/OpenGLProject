209cc2e0033ec2e669773f7c2b009544353cf5be
Norbert <Norbert@MSI>
2025-12-24 18:22:13 +0100

    drawing scene

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -20,131 +20,125 @@ using namespace std;
 
 int main(void)
 {
-    GLFWwindow* window;
-    GLFWmonitor* monitor;
-    const GLFWvidmode* mode;
-    Scene scene;
-    DrawingHelper drawingHelper;
+	GLFWwindow* window;
+	GLFWmonitor* monitor;
+	Scene scene;
+	DrawingHelper drawingHelper;
+	int screenWidth = 1920;
+	int screenHeight = 1080;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    // SET THE ERROR CALLBACK
-    glfwSetErrorCallback(error_callback);
+	// SET THE ERROR CALLBACK
+	glfwSetErrorCallback(error_callback);
 
-    //Setup the monitor
-    monitor = glfwGetPrimaryMonitor();
-    mode = glfwGetVideoMode(monitor);
+	//Setup the monitor
+	monitor = glfwGetPrimaryMonitor();
 
-    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(mode->width, mode->height, "Liszaj", monitor, NULL);
+	window = glfwCreateWindow(screenWidth, screenHeight, "Liszaj", monitor, NULL);
 
 
-    // IF window is not created, terminate
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	// IF window is not created, terminate
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
-    // load GLAD
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	// load GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
-    glViewport(0, 0, mode->width, mode->height);
+	glViewport(0, 0, screenWidth, screenHeight);
 
-    //CALLBACKS
-    glfwSetKeyCallback(window, key_callback);
-
-
-    ////////////////////////////////////////////////////////////////////////////////////
-    ///                        TEXTURES  & SHADERS                                   ///
-    ////////////////////////////////////////////////////////////////////////////////////
-    string shaderDir = filesystem::current_path().string() + "/src/shaders/";
-    string texturteDir = filesystem::current_path().string() + "/src/textures/";
-    
-    Shader textureShader = drawingHelper.setupShaderProgram(shaderDir, "default");
-
-    std::string fullPath = texturteDir + "tile933.png";
-    std::cout << "laduje teksture " << fullPath << std::endl;
-
-    Texture textures[]
-    {
-        Texture((texturteDir + "floor_tile.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE),
-        Texture((texturteDir + "wall_left.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE),
-        Texture((texturteDir + "wall.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE),
-        Texture((texturteDir + "wall_right.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE),
-    };
-    ////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////
-
-    drawingHelper.floorTiles(textures, scene);
-    drawingHelper.spawnArea(textures, scene);
+	//CALLBACKS
+	glfwSetKeyCallback(window, key_callback);
 
 
-    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    glm::vec3 lightPos = glm::vec3(0.5f, 50.0f, 0.5f);
-    glm::mat4 lightModel = glm::mat4(1.0f);
-    lightModel = glm::translate(lightModel, lightPos);
+	////////////////////////////////////////////////////////////////////////////////////
+	///                        TEXTURES  & SHADERS                                   ///
+	////////////////////////////////////////////////////////////////////////////////////
+	string shaderDir = filesystem::current_path().string() + "/src/shaders/";
+	string texturteDir = filesystem::current_path().string() + "/src/textures/";
 
-    glm::vec3 objectPos = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::mat4 objectModel = glm::mat4(1.0f);
-    objectModel = glm::translate(objectModel, objectPos);
+	Shader textureShader = drawingHelper.setupShaderProgram(shaderDir, "default");
 
-    textureShader.Activate();
-    glUniformMatrix4fv(glGetUniformLocation(textureShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(objectModel));
-    glUniform4f(glGetUniformLocation(textureShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-    glUniform3f(glGetUniformLocation(textureShader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	std::string fullPath = texturteDir + "tile933.png";
+	std::cout << "laduje teksture " << fullPath << std::endl;
+
+	Texture textures[]
+	{
+		Texture((texturteDir + "floor_tile.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE),
+		Texture((texturteDir + "wall.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE),
+	};
+	////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////
+
+	drawingHelper.floorTiles(textures, scene);
+	drawingHelper.spawnArea(textures, scene);
 
 
-    // Enables the Depth Buffer
-    glEnable(GL_DEPTH_TEST);
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 50.0f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
 
-    // Creates camera object
-    Camera camera(mode->width, mode->height, glm::vec3(0.5f, 1.6f, 0.5f));
+	glm::vec3 objectPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 objectModel = glm::mat4(1.0f);
+	objectModel = glm::translate(objectModel, objectPos);
+
+	textureShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(textureShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(objectModel));
+	glUniform4f(glGetUniformLocation(textureShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(textureShader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        // Handles camera inputs
-        camera.Inputs(window);
-        // Updates and exports the camera matrix to the Vertex Shader
-        camera.updateMatrix(45.0f, 0.1f, 100.0f);
+	// Enables the Depth Buffer
+	glEnable(GL_DEPTH_TEST);
 
-        // Draws different meshes
-        drawingHelper.drawScene(textureShader, scene, camera);
+	// Creates camera object
+	Camera camera(screenWidth, screenHeight, glm::vec3(0.5f, 1.6f, 0.5f));
 
-        // Swap the back buffer with the front buffer
-        glfwSwapBuffers(window);
-        // Take care of all GLFW events
-        glfwPollEvents();
-    }
 
-    // Delete all the objects we've created
-    textureShader.Delete();
-    for (auto mesh : scene.textureMeshes) {
-        scene.RemoveTextureMesh(mesh);
-    }
-    // Delete window before ending the program
-    glfwDestroyWindow(window);
-    // Terminate GLFW before ending the program
-    glfwTerminate();
-    return 0;
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		// Handles camera inputs
+		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+		// Draws different meshes
+		drawingHelper.drawScene(textureShader, scene, camera);
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete all the objects we've created
+	textureShader.Delete();
+	for (auto mesh : scene.textureMeshes) {
+		scene.RemoveTextureMesh(mesh);
+	}
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
 }
--- OpenGLProject/src/DrawingHelper.cpp
+++ OpenGLProject/src/DrawingHelper.cpp
@@ -2,85 +2,100 @@
 
 Shader DrawingHelper::setupShaderProgram(const std::string& shaderDir, const std::string& name) const
 {
-    // Œcie¿ki do plików vertex + fragment
-    std::string vertexPath = shaderDir + name + ".vert";
-    std::string fragmentPath = shaderDir + name + ".frag";
+	// Œcie¿ki do plików vertex + fragment
+	std::string vertexPath = shaderDir + name + ".vert";
+	std::string fragmentPath = shaderDir + name + ".frag";
 
-    // Tworzymy shader i zwracamy
-    Shader shaderProgram(vertexPath.c_str(), fragmentPath.c_str());
-    return shaderProgram;
+	// Tworzymy shader i zwracamy
+	Shader shaderProgram(vertexPath.c_str(), fragmentPath.c_str());
+	return shaderProgram;
 }
 
 void DrawingHelper::drawScene(Shader& textureShader, Scene& scene, Camera& camera)
 {
-    glClearColor(0.53f, 0.68f, 0.92f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.53f, 0.68f, 0.92f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    if (!scene.textureMeshes.empty()) {
-        for (Mesh* m : scene.textureMeshes)
-        {
-            m->Draw(textureShader, camera);
-        }
-    }
+	if (!scene.textureMeshes.empty()) {
+		for (Mesh* m : scene.textureMeshes)
+		{
+			m->Draw(textureShader, camera);
+		}
+	}
 }
 
 
 void DrawingHelper::floorTiles(Texture textures[], Scene& scene)
 {
-    float repeatX = 100.0f;
-    float repeatZ = 100.0f;
+	float repeatX = 100.0f;
+	float repeatZ = 100.0f;
 
-    Vertex vertices[] =
-    {
-        Vertex{glm::vec3(0.0f, 0.0f, 100.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(0.0f, 0.0f)},
-        Vertex{glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(0.0f, repeatZ)},
-        Vertex{glm::vec3(100.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(repeatX, repeatZ)},
-        Vertex{glm::vec3(100.0f, 0.0f, 100.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(repeatX, 0.0f)}
-    };
+	Vertex vertices[] =
+	{
+		Vertex{glm::vec3(0.0f, 0.0f, 100.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(0.0f, 0.0f)},
+		Vertex{glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(0.0f, repeatZ)},
+		Vertex{glm::vec3(100.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(repeatX, repeatZ)},
+		Vertex{glm::vec3(100.0f, 0.0f, 100.0f), glm::vec3(1.0f), glm::vec3(0,1,0), glm::vec2(repeatX, 0.0f)}
+	};
 
-    GLuint indices[] =
-    {
-        0, 1, 2,
-        0, 2, 3
-    };
+	GLuint indices[] =
+	{
+		0, 1, 2,
+		0, 2, 3
+	};
 
-    std::vector<Vertex> verts(vertices, vertices + sizeof(vertices) / sizeof(Vertex));
-    std::vector<GLuint> ind(indices, indices + sizeof(indices) / sizeof(GLuint));
-    std::vector<Texture> tex(textures, textures + sizeof(textures) / sizeof(Texture));
+	std::vector<Vertex> verts(vertices, vertices + sizeof(vertices) / sizeof(Vertex));
+	std::vector<GLuint> ind(indices, indices + sizeof(indices) / sizeof(GLuint));
+	std::vector<Texture> tex(textures, textures + sizeof(textures) / sizeof(Texture));
 
-    Mesh* tile = new Mesh(verts, ind, tex); // dynamiczne tworzenie
-    scene.AddTextureMesh(tile);
+	Mesh* tile = new Mesh(verts, ind, tex); // dynamiczne tworzenie
+	scene.AddTextureMesh(tile);
 }
 
 void DrawingHelper::spawnArea(Texture textures[], Scene& scene)
 {
-    float height = 3.0f;
-    float wLeft = 1.0f;
-    float wCenter = 6.0f;
-    float wRight = 2.0f;
+	float height = 3.0f;
 
-    float x0 = 0.0f;
-    float x1 = x0 + wLeft;
-    float x2 = x1 + wCenter;
-    float x3 = x2 + wRight;
+	// --- WALL SEGMENT ----------------------------------------------------
 
-    // --- LEFT WALL SEGMENT ----------------------------------------------------
-    {
-        Vertex verticies[] = {
-            { {x0, 0, 0}, {1,1,1}, {0,0,1}, {0,2} },
-            { {x0, height, 0}, {1,1,1}, {0,0,1}, {0,0} },
-            { {x1, height, 0}, {1,1,1}, {0,0,1}, {1,0} },
-            { {x1, 0, 0}, {1,1,1}, {0,0,1}, {1,2} }
-        };
+	Vertex wallsVerts[] =
+	{
+		// tylna
+		{ {0, 0, 0},   {1,1,1}, {0,0,1}, {0, 3} },
+		{ {0, 3, 0},   {1,1,1}, {0,0,1}, {0, 0} },
+		{ {10, 3, 0},  {1,1,1}, {0,0,1}, {10, 0} },
+		{ {10, 0, 0},  {1,1,1}, {0,0,1}, {10, 3} },
+		// prawa
+		{ {10, 3, 0},   {1,1,1}, {0,0,1}, {0, 0} },
+		{ {10, 0, 0},   {1,1,1}, {0,0,1}, {0, 3} },
+		{ {10, 0, 10},  {1,1,1}, {0,0,1}, {10, 3} },
+		{ {10, 3, 10},  {1,1,1}, {0,0,1}, {10, 0} },
+		// lewa
+		{ {0, 3, 0},   {1,1,1}, {0,0,1}, {0, 0} },
+		{ {0, 0, 0},   {1,1,1}, {0,0,1}, {0, 3} },
+		{ {0, 0, 10},  {1,1,1}, {0,0,1}, {10, 3} },
+		{ {0, 3, 10},  {1,1,1}, {0,0,1}, {10, 0} },
 
-        GLuint indicies[] = { 0,1,2, 0,2,3 };
+	};
 
-        std::vector<Vertex> verts(verticies, verticies + sizeof(verticies) / sizeof(Vertex));
-        std::vector<GLuint> ind(indicies, indicies + sizeof(indicies) / sizeof(GLuint));
-        std::vector<Texture> tex = { textures[1] };
+	GLuint wallIndiecies[] =
+	{
+		// tylna
+		0,1,2,
+		0,2,3,
+		// prawa
+		4, 5, 6,
+		4, 6, 7,
+		8,9,10,
+		8,10,11
+	};
 
-        Mesh* leftSegment = new Mesh(verts, ind, tex);
-        scene.AddTextureMesh(leftSegment);
-    }
+	std::vector<Vertex> verts(wallsVerts, wallsVerts + sizeof(wallsVerts) / sizeof(Vertex));
+	std::vector<GLuint> indicies(wallIndiecies, wallIndiecies + sizeof(wallIndiecies) / sizeof(GLuint));
+	std::vector<Texture> tex = { textures[1] };
+
+	Mesh* wall = new Mesh(verts, indicies, tex);
+	scene.AddTextureMesh(wall);
+
 }
 
